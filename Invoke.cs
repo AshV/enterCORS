8dc8f84bc5049391607fa41071666b48686ef034
Ashish Vishwakarma <Hello@AshishVishwakarma.com>
2022-01-16 19:55:16 +0530

    removed OpenApi

--- Invoke.cs
+++ Invoke.cs
@@ -1,56 +1,36 @@
 using System;
 using System.IO;
-using System.Net;
-using System.Net.Http;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
-using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Attributes;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
-using Microsoft.OpenApi.Models;
 using Newtonsoft.Json;
 
 namespace enterCORS
 {
-    public class Invoke
+    public static class Invoke
     {
-        private readonly ILogger<Invoke> _logger;
-
-        public Invoke(ILogger<Invoke> log)
-        {
-            _logger = log;
-        }
-
         [FunctionName("Invoke")]
-        [OpenApiOperation(operationId: "Run", tags: new[] { "url" })]
-        [OpenApiParameter(name: "url", In = ParameterLocation.Path, Required = false, Type = typeof(string), Description = "URL")]
-        [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "text/plain", bodyType: typeof(string), Description = "The OK response")]
-        public async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = "api/{url?}")] HttpRequest req, string url)
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", "post", Route = "api/{url?}")] HttpRequest req,
+            string url,
+            ILogger log)
         {
-            _logger.LogInformation("C# HTTP trigger function processed a request.");
+            log.LogInformation("C# HTTP trigger function processed a request.");
 
-            if (req.Method.ToLowerInvariant().Equals("get"))
-            {
-                if (string.IsNullOrEmpty(url)) return new OkObjectResult("GET request is expecting a URL. Please refer documentation.");
+            string name = req.Query["name"];
 
-                if (!url.ToLowerInvariant().StartsWith("http")) url = "http://" + url;
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            dynamic data = JsonConvert.DeserializeObject(requestBody);
+            name = name ?? data?.name;
 
-                Uri finalUri;
-                if (!Uri.TryCreate(url, UriKind.Absolute, out finalUri)) return new OkObjectResult($"URL {finalUri} doesn't seem proper, please verify.");
+            string responseMessage = string.IsNullOrEmpty(name)
+                ? "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response."
+                : $"Hello, {name}. This HTTP triggered function executed successfully.";
 
-                var http = new HttpClient();
-                return new OkObjectResult(await http.GetStringAsync(finalUri));
-            }
-
-            if (req.Method.ToLowerInvariant().Equals("post"))
-            {
-            }
-
-            _logger.LogInformation("Execution Completed.");
-            return new OkObjectResult("Unsupported Request.");
+            return new OkObjectResult(responseMessage);
         }
     }
 }
